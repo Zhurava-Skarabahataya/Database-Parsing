1272c7e3f1013aedee5de96ae1d35316ac59a236
Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-07-08 20:15:14 +0300

    complete

--- src/by/epamtc/zhurava/task05/Main.java
+++ src/by/epamtc/zhurava/task05/Main.java
@@ -1,8 +1,7 @@
 package by.epamtc.zhurava.task05;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
